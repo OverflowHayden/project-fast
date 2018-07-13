c60e50dfabc12601b2c39230a57218dc6d216339
OverflowHayden <info@haydenlin.nl>
2018-07-13 16:16:09 +0200

    fancy app bar features

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,6 +1,9 @@
 import tippy from 'tippy.js'
 var Snackbar = require("snackbarlightjs");
 
+//----------------------------------------------------------------------------------
+// IE & Edge placeholder-shown fix
+//----------------------------------------------------------------------------------
 function placeholderPolyfill() {
     this.classList[this.value ? 'add' : 'remove']('placeholder-hidden');
 }
@@ -13,7 +16,72 @@ if ((!(document.documentMode) && window.StyleMedia) || ((/*@cc_on!@*/false) || (
     });
 }
 
+
+//----------------------------------------------------------------------------------
+// Tippy tooltips
+//----------------------------------------------------------------------------------
 tippy('[title]', {
     size: 'large',
     performance: false
 })
+
+
+//----------------------------------------------------------------------------------
+// App Bar
+//----------------------------------------------------------------------------------
+if(document.querySelector('.app-bar')) {
+    var appBar = document.querySelector('.app-bar');
+    var scrollPosition = 0;
+    var scrollDirection = 0;
+    var ticking = false;
+
+    if(appBar.classList.contains('app-bar--prominent')) {
+        document.body.classList.add('with-app-bar--prominent');
+    }
+    else if(appBar.classList.contains('app-bar--dense')) {
+        document.body.classList.add('with-app-bar--dense');
+    }
+    else if(appBar.classList.contains('app-bar--fixed')) {
+        document.body.classList.add('with-app-bar--fixed');
+    }
+    else {
+        document.body.classList.add('with-app-bar');
+    }
+
+    if(!document.body.classList.contains('with-app-bar--fixed')) {
+        window.addEventListener('scroll', function (e) {
+            scrollDirection = window.scrollY - scrollPosition;
+
+            scrollPosition = window.scrollY;
+
+            if (!ticking) {
+
+                window.requestAnimationFrame(function () {
+                    appBarScroll(scrollDirection);
+                    ticking = false;
+                });
+
+                ticking = true;
+
+            }
+        });
+    }
+}
+
+function appBarScroll(scrollDirection) {
+    var appBar = document.querySelector('.app-bar');
+    var appBarHeight = parseInt(window.getComputedStyle(appBar).height, 10);
+    var position = parseInt(window.getComputedStyle(appBar).top, 10);
+
+    console.log(appBarHeight)
+
+    if(position - scrollDirection < -appBarHeight) {
+        appBar.style.top = '-' + appBarHeight + 'px';
+    }
+    else if(position - scrollDirection > 0) {
+        appBar.style.top = '0px';
+    }
+    else {
+        appBar.style.top = position - scrollDirection + 'px';
+    }
+}
--- src/materials/organisms/app-bar.md
+++ src/materials/organisms/app-bar.md
@@ -3,9 +3,44 @@ name: App Bar
 category: Organisms
 ---
 
-
+### Default
 ```app-bar.html
 <header class="app-bar">
+    <button class=app-bar__nav-toggle><i class="mdi mdi-menu"></i></button>
+    <span class="app-bar__title">App Bar title</span>
+    <div class="app-bar__actions">
+        <a href="#" class="button button--icon button--text"><i class="mdi mdi-printer"></i></a>
+        <a href="#" class="button button--icon button--text"><i class="mdi mdi-dots-vertical"></i></a>
+    </div>
+</header>
+```
+
+### Prominent
+```app-bar-prominent.html
+<header class="app-bar app-bar--prominent">
+    <button class=app-bar__nav-toggle><i class="mdi mdi-menu"></i></button>
+    <span class="app-bar__title">App Bar title</span>
+    <div class="app-bar__actions">
+        <a href="#" class="button button--icon button--text"><i class="mdi mdi-printer"></i></a>
+        <a href="#" class="button button--icon button--text"><i class="mdi mdi-dots-vertical"></i></a>
+    </div>
+</header>
+```
+
+### Dense
+```app-bar-dense.html
+<header class="app-bar app-bar--dense">
+    <button class=app-bar__nav-toggle><i class="mdi mdi-menu"></i></button>
+    <span class="app-bar__title">App Bar title</span>
+    <div class="app-bar__actions">
+        <a href="#" class="button button--icon button--text"><i class="mdi mdi-printer"></i></a>
+        <a href="#" class="button button--icon button--text"><i class="mdi mdi-dots-vertical"></i></a>
+    </div>
+</header>
+```
+### Fixed
+```app-bar-fixed.html
+<header class="app-bar app-bar--fixed">
     <button class=app-bar__nav-toggle><i class="mdi mdi-menu"></i></button>
     <span class="app-bar__title">App Bar title</span>
     <div class="app-bar__actions">
