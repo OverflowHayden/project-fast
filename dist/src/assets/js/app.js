063bd6338275867798607c4bbea827b888ccbe01
Hayden Lin <info@haydenlin.nl>
2019-01-04 16:18:39 +0100

    added upload-field

--- dist/src/assets/js/app.js
+++ dist/src/assets/js/app.js
@@ -653,3 +653,26 @@ if(document.querySelector('.fab--menu')) {
         this.focus();
     });
 }
+
+//----------------------------------------------------------------------------------
+// Upload field value
+//----------------------------------------------------------------------------------
+if(document.querySelectorAll('.upload-field').length > 0) {
+    var uploadFields = document.querySelectorAll('.upload-field');
+
+    uploadFields.forEach(function(field) {
+        if (field !== null) {
+
+            field.querySelector('.upload-field__input').addEventListener('change', function() {
+                var values = [];
+                
+                for (let i = 0, files = this.files.length; i < files; i++) {
+                    const file = this.files[i];
+                    values.push(file.name);
+                }
+
+                field.querySelector('.upload-field__value').innerHTML = values.join();
+            });
+        }
+    });
+}
